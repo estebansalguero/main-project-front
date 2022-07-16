b43e06d2c99fc7ff1da67c1cd224b1c10eb13166
Gabriel Monge <gabriel.monge.lizano@gmail.com>
2022-07-16 00:26:21 -0600

    Testing with back end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from "react";
-import logo from './logo.svg';
-import './App.css';
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import {useEffect, useState} from "react";
 
 function App() {
+  const [initialstate, setInitialState] = React.useState([]);
+  const [state, setState] = useState([]);
 
-  const [data, setData] = React.useState(null);
+  useEffect(() => {
+    fetch("/ping")
+      .then(res => res.json())
+      .then(data => {
+        setInitialState(data);
+      }
+      );
+  }
+  , []);
 
-  React.useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
-  console.log(data);
+  useEffect(() => {
+    getData();
+  }
+  , []);
+
+  async function getData() {
+    let name = document.getElementById("nameIn").value;
+    const response = await fetch(`/crud?name=${name}`);
+    const data = await response.json();
+    setState(data);
+  }
+
+  function showName() {
+    getData();
+    document.getElementById("name").innerHTML = state[0];
+  }
 
   return (
-
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        <p>{initialstate.status}</p>
+        <input type="text" id="nameIn" />
+        <button onClick={showName}>Search User</button>
+        <p id="name"></p>
       </header>
     </div>
   );
